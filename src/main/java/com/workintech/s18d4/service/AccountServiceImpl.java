db1c49094c3c21c69b4d5b8f19964c7b720e9225
Evren Aydın <96618847+evren-aydin@users.noreply.github.com>
2024-07-09 16:25:21 +0300

    f-1

---
+++ src/main/java/com/workintech/s18d4/service/AccountServiceImpl.java
@@ -0,0 +1,7 @@
+package com.workintech.s18d4.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountServiceImpl implements AccountService{
+}
---
+++ src/main/java/com/workintech/s18d4/service/AddressServiceImpl.java
@@ -0,0 +1,8 @@
+package com.workintech.s18d4.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AddressServiceImpl implements AddressService{
+}
